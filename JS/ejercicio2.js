f7cc74fe667812070fbead655be713d693e21dd1
Alex Benites E. <66579129+abenitese@users.noreply.github.com>
2024-10-01 00:21:21 -0500

    Add files via upload

---
+++ JS/ejercicio2.js
@@ -0,0 +1,37 @@
+var fechaIngreso = prompt("Ingrese la Fecha","mm/dd/yyyy");
+var fecha=new Date(fechaIngreso);
+
+var dia=fecha.getDay();
+
+var diaSemana;
+
+switch (dia) {
+    case 0:
+    diaSemana="Domingo";
+    break;
+
+    case 1:
+    diaSemana="Lunes";
+    break;
+
+    case 2:
+    diaSemana="Martes";
+    break;
+
+    case 3:
+    diaSemana="Miercoles";
+    break;
+
+    case 4:
+    diaSemana="Jueves";
+    break;
+
+    case 5:
+    diaSemana="Viernes";
+    break;
+
+    case 6:
+    diaSemana="Sabado";
+    break;
+}
+alert("El dia de la Semana de esa Fecha es: "+diaSemana);
---
+++ JS/ejercico1.js
@@ -0,0 +1,9 @@
+var numero1= parseInt(window.prompt("INGRESE PRIMER NUMERO"));
+var numero2= parseInt(window.prompt("INGRESE SEGUNDO NUMERO"));
+
+//determinar el mayor
+if (numero1>numero2) {
+    alert("EL MAYOR ES EL NUMERO: "+numero1);
+} else {
+    alert("EL MAYOR ES EL NUMERO: "+numero2);
+}
